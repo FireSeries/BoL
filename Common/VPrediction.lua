8dc3e399b214d0dd8d2e29e3e38784f858db8728
FireSeries <M.Schalla93@gmail.com>
2014-05-29 07:52:13 +0200

    Update VPrediction.lua
    Disabled auto updater

--- Common/VPrediction.lua
+++ Common/VPrediction.lua
@@ -1,6 +1,6 @@
 local version = "2.51"
 local TESTVERSION = false
-local AUTOUPDATE = true
+local AUTOUPDATE = false
 local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/Hellsing/BoL/master/common/VPrediction.lua".."?rand="..math.random(1,10000)
 local UPDATE_FILE_PATH = LIB_PATH.."vPrediction.lua"
